9ee64c722ab1d249751b90c80e345c887d3ebf92
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2020-03-12 13:36:41 -0700

    Add files via upload

--- JDBC.sql
+++ JDBC.sql
@@ -1,24 +1,24 @@
 CREATE TABlE WritingGroups(
-GroupName VARCHAR(20) NOT NULL,
-HeadWriter VARCHAR(20) NOT NULL,
-YearFormed VARCHAR(20) NOT NULL,
-Subject VARCHAR (20) NOT NULL,
+GroupName VARCHAR(50) NOT NULL,
+HeadWriter VARCHAR(50) NOT NULL,
+YearFormed VARCHAR(50) NOT NULL,
+Subject VARCHAR (50) NOT NULL,
 constraint writing_groups_pk primary key(GroupName));
 
 CREATE TABLE Publishers(
-PublisherName VARCHAR(20) NOT NULL,
-PublisherAddress VARCHAR(20) NOT NULL,
-PublisherPhone VARCHAR(20) NOT NULL,
-PublisherEmail VARCHAR(20) NOT NULL,
+PublisherName VARCHAR(50) NOT NULL,
+PublisherAddress VARCHAR(50) NOT NULL,
+PublisherPhone VARCHAR(50) NOT NULL,
+PublisherEmail VARCHAR(50) NOT NULL,
 constraint publisher_pk primary key(PublisherName)
 );
 
 CREATE TABLE Books(
-GroupName VARCHAR(20) NOT NULL,
-BookTitle VARCHAR(20) NOT NULL,
-PublisherName VARCHAR(20) NOT NULL,
-YearPublished VARCHAR(20) NOT NULL,
-NumberPages VARCHAR(20) NOT NULL,
+GroupName VARCHAR(50) NOT NULL,
+BookTitle VARCHAR(50) NOT NULL,
+PublisherName VARCHAR(50) NOT NULL,
+YearPublished VARCHAR(50) NOT NULL,
+NumberPages VARCHAR(50) NOT NULL,
 constraint publisher_fk foreign key(PublisherName) references Publishers(PublisherName),
 constraint writing_group_fk foreign key(GroupName) references WritingGroups(GroupName),
 constraint books_pk primary key(GroupName, BookTitle)
@@ -30,7 +30,15 @@ Insert into PUBLISHERS(PUBLISHERNAME, PUBLISHERADDRESS, PUBLISHERPHONE, PUBLISHE
 VALUES ('No Starch Press', '245 8th street', '800-420-7240', 'info@nostarch.com'),
 ('The MIT Press', 'One Rogers Street', '800-405-1619', 'info@mitpress.com'),
 ('Cambridge Press', '1 Liberty Plaza', '800-872-7423', 'info@cambridge.com'),
-('Oreilly Media', '1005 Gravenstein', '800-889-8969', 'info@oreilly.com')
+('Oreilly Media', '1005 Gravenstein', '800-889-8969', 'info@oreilly.com');
+
+Insert into WritingGroups(GROUPNAME, HEADWRITER, YEARFORMED, SUBJECT)
+VALUES ('CLRS', 'Thomas Cormen', '1986', 'Computer Science'), 
+ ('LinAlg4All', 'Gilbert Strang', '1975', 'Mathematics'),
+ ('SixDegrees', 'Jeremy Watts', '2015', 'Computer Science'),
+ ('Count Bayesie', 'Will Kurt', '2015', 'Mathematics'),
+ ('Database Design', 'Tom Jewett', '2002', 'Computer Science');
+
 
 -- sample commands 
 select * from WRITINGGROUPS -- List all writing groups
@@ -40,4 +48,5 @@ select * from PUBLISHERS -- list all publishers
 select * from PUBLISHERS where PUBLISHERNAME = 'No Starch Press' -- Select all the content of a particular publisher in this case No Starch Press
 
 select * from BOOKS -- list all book titles
+ 
 
